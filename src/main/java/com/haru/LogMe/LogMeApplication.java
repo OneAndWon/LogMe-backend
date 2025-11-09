267c6c3aeab2af34861b45c65159f087af80caad
hyew0 <lhy3899@gmail.com>
2025-11-09 20:11:26 +0900

    first commit

---
+++ src/main/java/com/haru/LogMe/LogMeApplication.java
@@ -0,0 +1,13 @@
+package com.haru.LogMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogMeApplication.class, args);
+	}
+
+}
