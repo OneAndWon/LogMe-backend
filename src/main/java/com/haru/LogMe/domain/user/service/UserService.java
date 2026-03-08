f6233773aa34bf588b27a6675a96b2e935472de1
hyew0 <lhy3899@gmail.com>
2026-03-08 22:04:23 +0900

    feat: user api created

--- src/main/java/com/haru/LogMe/domain/user/service/UserService.java
+++ src/main/java/com/haru/LogMe/domain/user/service/UserService.java
@@ -1,6 +1,8 @@
 package com.haru.LogMe.domain.user.service;
 
 import com.haru.LogMe.domain.user.dto.UserMeResponse;
+import com.haru.LogMe.domain.user.dto.UserSettingsDto;
+import com.haru.LogMe.domain.user.dto.UserUpdateRequest;
 import com.haru.LogMe.domain.user.entity.User;
 import com.haru.LogMe.domain.user.repository.UserRepository;
 import com.haru.LogMe.global.exception.CustomException;
@@ -16,10 +18,52 @@ public class UserService {
 
     private  final UserRepository userRepository;
 
+    // 1. 내 정보 조회
     public UserMeResponse getMyInfo(Long userId) {
         User user = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
         return UserMeResponse.from(user);
+    }
+
+    // 2. 내 정보 수정
+    @Transactional
+    public UserMeResponse updateMyInfo(Long userId, UserUpdateRequest request) {
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        user.updateNickname(request.getNickname());
+
+        return UserMeResponse.from(user);
+    }
+
+    // 3. 회원 탈퇴
+    @Transactional
+    public void deleteUser(Long userId) {
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        // Cascade.ALL 과 orphanRemoval=true 설정으로 UserSettings도 자동 삭제됨
+        userRepository.delete(user);
+    }
+
+    // 4. 사용자 설정 조회
+    public UserSettingsDto getUserSettings(Long userId) {
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        return UserSettingsDto.from(user.getUserSettings());
+    }
+
+    // 5. 사용자 설정 수정
+    @Transactional
+    public UserSettingsDto updateUserSettings(Long userId, UserSettingsDto request) {
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        user.getUserSettings().updateNotificationSettings(request.getNotificationSettings());
+
+        // 수정된 설정 데이터를 반환
+        return UserSettingsDto.from(user.getUserSettings());
     }
 }
